ad993cc5cb69f106ebfbdf4f793d856f687bbbb0
shubin.zheng <shubin.zheng@hotmail.com>
2021-05-10 19:55:22 +0800

    third commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello github");
+        System.out.println("hello git3");
     }
 }
