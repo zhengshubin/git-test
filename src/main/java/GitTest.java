99b9fcf028bf6d7aeadff248464bceaad8c1a853
zhengshubin <40193259+zhengshubin@users.noreply.github.com>
2021-05-10 22:59:20 +0800

    pull test2

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("push test");
         System.out.println("pull test");
+         System.out.println("pull test2");
         
     }
 }
